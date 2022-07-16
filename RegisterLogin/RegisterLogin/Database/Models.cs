76a6e02856e30da0c99238c9c8c852e1e19d47e3
Zeynalbey <zeynaleg@code.edu.az>
2022-07-16 19:40:31 +0400

    Models created

--- RegisterLogin/RegisterLogin/Database/Models.cs
+++ RegisterLogin/RegisterLogin/Database/Models.cs
@@ -8,6 +8,18 @@ namespace RegisterLogin.Database
 {
     class Models
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Models(string firstName, string lastName, string email, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
 
     }
 }
