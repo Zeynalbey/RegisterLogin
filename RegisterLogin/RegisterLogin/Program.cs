ba05e785477f2ad29f8502e014711f4fec2d09ee
Zeynalbey <zeynaleg@code.edu.az>
2022-07-16 19:20:09 +0400

    Program created

---
+++ RegisterLogin/RegisterLogin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RegisterLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Our commands:");
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+
+            string command = Console.ReadLine();
+
+            if (command == "/register")
+            {
+                
+            }
+            else if(command == "/login")
+            {
+                
+            }
+
+        }
+    }
+}
